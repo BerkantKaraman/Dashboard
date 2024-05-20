8ed01261f5ec878b663d74b432727a3c00bac1ed
Berkant Karaman <berkantkaraman01@gmail.com>
2024-05-20 16:02:58 +0300

    feat: base for the project and dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { transformAssetUrls } from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: true,
+
+  devtools: { enabled: true },
+
+  css: [
+    'vuetify/styles',
+    'vuetify/lib/styles/main.sass',
+    '~/assets/scss/app.scss',
+    '~/assets/fonts/poppins.css'
+  ],
+
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  vite: {
+    ssr: {
+      noExternal: ['vuetify'],
+    },
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@import "@/assets/scss/variables.scss"; @import "@/assets/scss/mixins.scss";',
+        },
+      },
+    },
+  },
 })
