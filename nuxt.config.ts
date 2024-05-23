da97e7a9578b1c1482e12a3d4479dd076ba4c766
Berkant Karaman <berkantkaraman01@gmail.com>
2024-05-23 14:43:54 +0300

    feat: vuetify autoImport, theme and settings created

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { transformAssetUrls } from 'vite-plugin-vuetify';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
   ssr: true,
@@ -7,6 +7,17 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   modules:[
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true,          
+          styles: {
+          configFile: './assets/scss/settings.scss',
+        },
+       }))
+      })
+    },
+
     '@nuxtjs/google-fonts', 
     '@nuxt/image'
   ],
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,11 +1,25 @@
+import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
+
 import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     ssr: true,
 
-    components: {},
+    theme: {
+      defaultTheme: "customTheme",
+      themes: {
+        customTheme: {
+          dark: false,
+          colors: {
+            primary: "#467c65",
+            textColor: "#5b5b5b",
+            textBackground: "#edf6f2",
+          },
+        },
+      },
+    },
   });
 
   nuxtApp.vueApp.use(vuetify);
